f736abfc02dd659f3799a1141511fc413e64b200
Mohammed Sitni <149593050+msitni1337@users.noreply.github.com>
2024-07-24 01:32:24 +0000

    Creating Sphere objects

--- includes/Object.h
+++ includes/Object.h
@@ -1,6 +1,6 @@
 #ifndef OBJECT_H
 #define OBJECT_H
-#include "Mat4x4.h"
+#include "Ray.h"
 
 typedef enum e_object_type
 {
@@ -12,9 +12,10 @@ typedef enum e_object_type
 typedef struct s_object
 {
 	t_object_type type;
+	t_hits (*intersection)(t_object *object, t_ray ray);
 	t_mat4x4 SRT_matrix;
 	t_mat4x4 ISRT_matrix;
-	t_vec4 color;
-	size_t object_data;
+	t_vec3 color;
+	float object_data;
 } t_object;
 #endif //SPHERE_H
---
+++ src/Sphere.c
@@ -0,0 +1,69 @@
+#include "Object.h"
+
+t_hits sphere_intersection(t_object *object, t_ray ray)
+{
+	/*
+		(r.a.x + t r.d.x)² + (r.a.y + t r.d.y)² + (r.a.z + t r.d.z)²  = r²
+		(r.a.x + t r.d.x)² + (r.a.y + t r.d.y)² + (r.a.z + t r.d.z)²  = r²
+
+		(r.a.x + t r.d.x)² = rax² + t² rdx² + 2 t * rax * rdx
+		(r.a.y + t r.d.y)² = ray² + t² rdy² + 2 t * ray * rdy
+		(r.a.z + t r.d.z)² = raz² + t² rdz² + 2 t * raz * rdz
+
+		rax² + t² rdx² + 2 t *  rax * rdx + ray² + t² rdy² + 2 t *  ray * rdy + raz² + t² rdz² + 2 t *  raz * rdz - r = 0
+		t² (rdx² + rdy² + rdz²) + 2 t (rax * rdx + ray * rdy + raz * rdz) + (rax² + ray² + raz² - r²) = 0
+
+		a = (rdx² + rdy² + rdz²) = > this always will be 1 cause ray.dir is normalized
+		b = 2 * (rax * rdx + ray * rdy + raz * rdz)
+		c = (rax² + ray² + raz² - r²) << Can be calculated & stored once instead of always calculating it hum ...
+
+		t = (-b +/- sqrt(b² - 4 a c)) / (2 * a)
+	*/
+	float radius_sq = (float)object->object_data;
+	t_vec3 map_origine = mat_mul_vec3(&object->ISRT_matrix, &ray.origin);
+	t_vec3 map_direct = mat_mul_vec3(&object->ISRT_matrix, &ray.dir);
+
+	float b = 2 * (map_origine.x * map_direct.x + map_origine.y * map_direct.y + map_origine.z * map_direct.z);
+	float c = (map_origine.x * map_origine.x + map_origine.y * map_origine.y + map_origine.z * map_origine.z - radius_sq);
+	float determinant = b * b - 4 * c;
+	t_hits hit;
+	hit.count = 0;
+	if (determinant == 0.0)
+	{
+		float t = -b / 2;
+		if (t < 0.1) // near clipping plane
+			return hit;
+		hit.count = 1;
+
+		hit.hit_point = vec3_scale(map_direct, t);
+		hit.hit_point = vec3_add_vec3(hit.hit_point, map_origine);
+		hit.hit_point = mat_mul_vec3(&object->SRT_matrix, &hit.hit_point);
+	}
+	else if (determinant > 0.0)
+	{
+		float t = (-b + sqrt(b * b - 4 * c)) / 2;
+		if (t < 0.1) // near clipping plane
+			return hit;
+		hit.count = 1;
+		hit.hit_point = vec3_scale(map_direct, t);
+		hit.hit_point = vec3_add_vec3(hit.hit_point, map_origine);
+		hit.hit_point = mat_mul_vec3(&object->SRT_matrix, &hit.hit_point);
+	}
+	return hit;
+}
+
+
+t_object new_sphere(t_vec3 pos, float radius, t_vec3 color)
+{
+    t_object sphere;
+
+    sphere.type = OBJ_SPHERE;
+    sphere.color = vec3_scale(color, 1.0f/255.0f);
+    sphere.intersection = &sphere_intersection;
+    sphere.object_data = radius;
+    sphere.SRT_matrix = mat_id();
+    *mat_at(&sphere.SRT_matrix, 0, 3) = pos.x;
+    *mat_at(&sphere.SRT_matrix, 1, 3) = pos.y;
+    *mat_at(&sphere.SRT_matrix, 2, 3) = pos.z;
+    sphere.ISRT_matrix = mat_inv(&sphere.SRT_matrix);
+}
